b94ad806e670851d50d5211293cd677b5fce498f
Wilker Xavier <willcostaxavier@hotmail.com>
2020-07-07 22:38:21 -0300

    Bubblesort

---
+++ OrdenadorBubbleSort.py
@@ -0,0 +1,7 @@
+def bolha(self, lista):
+    fim = len(lista)
+
+    for i in range(fim-1, 0, -1):
+        for j in range(i):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
